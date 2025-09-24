b7a147079dfbb8cd7afa8ca1cae30a6e5efa8081
traffic95 <traffic95@xeondev.com>
2025-09-24 12:46:08 +0200

    CNBetaWin2.3.4 support

--- protocol/src/root.zig
+++ protocol/src/root.zig
@@ -145,7 +145,7 @@ fn cmdNames(comptime T: type) [10_000]?[]const u8 {
 }
 
 pub const DummyMessage = struct {
-    pub const cmd_id = 1604;
+    pub const cmd_id = 8317;
     pb: protobuf.ProtobufMixins(@This()) = .{},
 
     pub const _desc_table = .{};
